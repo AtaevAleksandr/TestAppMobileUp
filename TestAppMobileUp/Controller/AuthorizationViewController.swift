b65cfa1d1e8d31d455be0fe0f612dfccbae7230a
Aleksandr Ataev <onlyaa94@mail.ru>
2023-04-21 15:32:25 +0300

    Initial commit

---
+++ TestAppMobileUp/Controller/AuthorizationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizationViewController.swift
+//  TestAppMobileUp
+//
+//  Created by Aleksandr Ataev on 21.04.2023.
+//
+
+import UIKit
+
+final class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
