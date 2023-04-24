80ae6fc44e2a1cda18ba8ee40ac2530d1302401f
Aleksandr Ataev <onlyaa94@mail.ru>
2023-04-24 19:02:54 +0300

    Reinstall VKSdkFramework

--- TestAppMobileUp/App/SceneDelegate.swift
+++ TestAppMobileUp/App/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import VKSdkFramework
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -18,6 +19,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.rootViewController = UINavigationController(rootViewController: AuthorizationViewController())
         window?.makeKeyAndVisible()
+    }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
